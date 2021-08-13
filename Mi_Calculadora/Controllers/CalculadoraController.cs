90adbfd9bb30c23c082f44a0207fa3a91da1d2c7
JackoCol <ccja1985@gmail.com>
2021-08-12 22:36:48 -0500

    CReacion Calculadoracontroller

---
+++ Mi_Calculadora/Controllers/CalculadoraController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mi_Calculadora.Models;
+
+namespace Mi_Calculadora.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public CalculadoraController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
