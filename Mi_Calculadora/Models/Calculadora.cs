91d6e830e72c41a2ecf0aba0dfefa2920c7be361
JackoCol <ccja1985@gmail.com>
2021-08-12 22:45:37 -0500

    Calculadora

---
+++ Mi_Calculadora/Models/Calculadora.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Mi_Calculadora.Models
+{
+    public class CalculadoraViewModel
+    {
+        public int  operador1 { get; set; }
+
+
+        public int operador2 { get; set; }
+
+
+        public string Accion { get; set; }
+
+
+        public int Respuesta { get; set; }
+
+
+
+
+       
+    }
+}
